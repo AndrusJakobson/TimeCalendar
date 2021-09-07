eddb00df605c70ead79d02ed46f0c6799a0ef80b
anjako <anjako@ttu.ee>
2021-09-07 23:21:00 +0300

    Swappable menu buttons

--- app/src/main/java/ee/andrusj/timecalendar/MainActivity.kt
+++ app/src/main/java/ee/andrusj/timecalendar/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.PersistableBundle
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.commit
 import androidx.fragment.app.replace
@@ -12,7 +13,9 @@ import ee.andrusj.timecalendar.data.database.AppDatabase
 import ee.andrusj.timecalendar.ui.main.view.MainFragment
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
-    val database by lazy { AppDatabase.getDatabase(this) }
+    private val database by lazy { AppDatabase.getDatabase(this) }
+    private lateinit var menuButton: MenuItem
+    private lateinit var menuCloseButton: MenuItem
 
 
     override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
@@ -33,12 +36,20 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu_action_bar, menu)
+        menuButton = menu?.findItem(R.id.action_bar_menu)!!
+        menuCloseButton = menu.findItem(R.id.action_bar_menu_close)
         return super.onCreateOptionsMenu(menu)
     }
 
     override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
         R.id.action_bar_menu -> {
-            Log.d("yemen", "Menu is clicked")
+            menuButton.isVisible = false
+            menuCloseButton.isVisible = true
+            true
+        }
+        R.id.action_bar_menu_close -> {
+            menuButton.isVisible = true
+            menuCloseButton.isVisible = false
             true
         }
         else -> {
